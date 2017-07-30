f91f0c558a0e4f5349648a1a79fe6863fee2487c
emag <lanabe.lanabe@gmail.com>
2017-07-31 07:46:08 +0900

    Until P89

---
+++ programming-in-scala/src/main/scala/chap10/Element1.scala
@@ -0,0 +1,32 @@
+package chap10
+
+abstract class Element1 {
+
+  def contents: Array[String]
+
+  /*
+   * フィールドとしての表現。どちらにしてもクライアントのコードは一緒
+   * フィールドアクセスの方がメソッド呼び出しよりわずかに速い
+   * メモリはフィールドのほうが使う
+   */
+//  val height = contents.length
+  def height: Int = contents.length
+
+  def width: Int =
+    if (height == 0) 0
+    else contents(0).length
+}
+
+class ArrayElement(conts: Array[String]) extends Element1 {
+
+  override def contents: Array[String] = conts
+
+}
+
+object Element1Client extends App {
+
+  val element = new ArrayElement(Array("foo", "barbar"))
+  println(s"element [$element]")
+  println(s"element.height [${element.height}]")
+  println(s"element.width  [${element.width}]")
+}
